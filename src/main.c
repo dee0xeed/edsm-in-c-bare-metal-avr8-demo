d1915ae6f7c5adef49ece71442e658c9a680c602
zed <dee0xeed@gmail.com>
2023-12-02 10:30:14 +0300

    On branch main modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -7,18 +7,17 @@
 
 int main(void)
 {
-	cli();
-	init_message_queue();
-	start_state_machines();
-//	init_software();
-//	init_hardware();
-//	usart_prep_rx();
-	sei();
+    cli();
+    init_message_queue();
+    start_state_machines();
+//  init_software();
+//  init_hardware();
+//  usart_prep_rx();
+    sei();
     sleep_enable();
 
-	for (;;) {
-		process_messages();
-		//sleep_mode();
-		sleep_cpu();
-	}
+    for (;;) {
+        process_messages();
+        sleep_cpu();
+    }
 }
